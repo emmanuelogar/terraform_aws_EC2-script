70f8875d5fc08fbcaabf342dd24cc1d2c9331c27
Emmanuel Ogar <84130927+emmanuelogar@users.noreply.github.com>
2024-02-11 03:37:58 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
+resource "aws_s3_bucket" "oiueeieoo9934" {
+  acl    = "private"
+  versioning = "enabled"
+
+  tags = {
+    Name = oiueeieoo9934"
   }
 }
-
-resource "aws_instance" "" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
-
- }
