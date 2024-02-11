92c75264623dccf3551fe309abcac85b075a3171
Emmanuel Ogar <84130927+emmanuelogar@users.noreply.github.com>
2024-02-11 23:59:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,22 @@
-resource "aws_s3_bucket" "oiueeieoo9934" {
-  acl    = "private"
-  versioning = "enabled"
+variable "ami" {
+  type = string
+  default = "ami-0123456789abcdef0" # Replace with actual AMI ID
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro" # Replace with desired instance type
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
   tags = {
-    Name = oiueeieoo9934"
+    Name = "harness-test-ec2"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.ec2_instance.public_ip
 }
